1629ef1c03c878d0af4e78a94c9d5556e90a1d5a
thegirlcoderr <lynndy2015@gmail.com>
2023-07-25 13:01:54 +0100

    added spinner and display popular movies function

--- js/script.js
+++ js/script.js
@@ -37,6 +37,41 @@ async function displayPopularMovies() {
     });
 }
 
+//display popular tv shows
+async function displayPopularShows() {
+    const { results } = await fetchAPIData('tv/popular');
+    results.forEach(show => {
+        const div = document.createElement('div');
+        div.classList.add('card')
+        div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+         ${
+            show.poster_path
+            ?  `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            :` <img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            
+         }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+
+        document.querySelector('#popular-shows').appendChild(div);
+    });
+}
+
 // FETCH data from TMDB 
 
  async function fetchAPIData(endpoint) {
@@ -44,15 +79,25 @@ async function displayPopularMovies() {
      const API_URL = 'http://api.themoviedb.org/3';
      
 
+     showSpinner();
 
      const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`);
      const data = await response.json();
    
 
 
+     hideSpinner();
      return data;
  }
 
+
+//adding and hiding spinner
+function showSpinner() {
+     document.querySelector('.spinner').classList.add('show')
+}
+ function hideSpinner() {
+     document.querySelector('.spinner').classList.remove('show')
+ }
 
 //show active link
 function  showActiveLink() {
@@ -80,7 +125,7 @@ function init() {
             console.log('Movie Details');
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularShows();
             break;
         case '/tv-details.html':
             console.log('TV Details');
@@ -95,7 +140,7 @@ function init() {
 
 document.addEventListener('DOMContentLoaded', init);
 
-//deleted dummy content
+
 
 
 
