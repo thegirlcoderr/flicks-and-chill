949aae5acea75cd552afe94f3106eaa0b6ebeca0
thegirlcoderr <lynndy2015@gmail.com>
2023-07-25 12:33:15 +0100

    added display popular movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,57 @@ const state = {
     currentPage: window.location.pathname
 };
 
+//display popular movies
+async function displayPopularMovies() {
+    const { results } = await fetchAPIData('movie/popular');
+    results.forEach(movie => {
+        const movieDiv = document.createElement('div');
+        movieDiv.classList.add('card')
+        movieDiv.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+         ${
+            movie.poster_path
+            ?  `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            :` <img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            
+         }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+
+        document.querySelector('#popular-movies').appendChild(movieDiv);
+    });
+}
+
+// FETCH data from TMDB 
+
+ async function fetchAPIData(endpoint) {
+     const API_KEY = 'e3bcd3ddedc1aeb5f13eda82709a14e1';
+     const API_URL = 'http://api.themoviedb.org/3';
+     
+
+
+     const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`);
+     const data = await response.json();
+   
+
+
+     return data;
+ }
+
 
 //show active link
 function  showActiveLink() {
@@ -23,7 +74,7 @@ function init() {
     switch (state.currentPage) {
         case '/':
         case '/index.html':
-            console.log('Home');   
+           displayPopularMovies();   
             break;
         case '/movie-details.html':
             console.log('Movie Details');
@@ -43,6 +94,8 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+//deleted dummy content
 
 
 
