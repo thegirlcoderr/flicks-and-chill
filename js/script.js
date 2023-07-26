23a38192bd44092b2d7bb3e52e98addefcb23109
thegirlcoderr <lynndy2015@gmail.com>
2023-07-26 17:09:21 +0100

    added Swiper slider

--- js/script.js
+++ js/script.js
@@ -206,13 +206,6 @@ async function displayShowDetails() {
 }
 
 
-
-
-
-
-
-
-
 //To Display Backdrop to Movie/show Details page
 function displayBackgroundImage(type,backgroundPath) {
     const overlayDiv = document.createElement('div');
@@ -235,6 +228,56 @@ function displayBackgroundImage(type,backgroundPath) {
     }
 }
 
+
+//To Display Slider Movies
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing');
+    console.log(results);
+
+    results.forEach(movie => {
+        const sliderDiv = document.createElement('div');
+        sliderDiv.classList.add('swiper-slide');
+        sliderDiv.innerHTML = `
+           <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} /10
+            </h4>
+     
+     ` ;
+        
+        document.querySelector('.swiper-wrapper').appendChild(sliderDiv);
+
+        initSwiper();
+    });
+}
+
+function initSwiper() {
+     const swiper = new Swiper('.swiper', {
+         slidesPerView : 1,
+         spaceBetween: 30,
+         freeMode: true,
+         loop: true,
+         autoplay: {
+             delay: 4000,
+             disableOnInteraction: false
+         },
+         breakpoints: {
+             500: {
+                 slidesPerView: 2,
+             },
+              700: {
+                 slidesPerView: 3,
+             },
+               1200: {
+                 slidesPerView: 4,
+             },
+         }
+
+     })
+ }
+ 
 
 
 // FETCH data from TMDB 
@@ -286,6 +329,7 @@ function init() {
     switch (state.currentPage) {
         case '/':
         case '/index.html':
+            displaySlider();
            displayPopularMovies();   
             break;
         case '/movie-details.html':
@@ -298,7 +342,7 @@ function init() {
             displayShowDetails();
             break;
         case '/search.html':
-            console.log('Search');
+            search();
             break; 
     }
 
