bfb9a592864c9574e411793f3b6d40e36449c006
thegirlcoderr <lynndy2015@gmail.com>
2023-07-26 09:11:23 +0100

    added backdrop overlay to movie-details

--- js/script.js
+++ js/script.js
@@ -72,12 +72,14 @@ async function displayPopularShows() {
     });
 }
 
-//Display Movie Details
+// Display Movie Details
 async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1];
-    console.log(movieId);
-
     const movie = await fetchAPIData(`movie/${movieId}`);
+    
+    //backdrop image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
     div.innerHTML = `
      <div class="details-top">
@@ -134,6 +136,29 @@ async function displayMovieDetails() {
     document.querySelector('#movie-details').appendChild(div);
 
 }
+//To Display Backdrop to Movie Details page
+function displayBackgroundImage(type,backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
+}
+
+
 
 // FETCH data from TMDB 
 
